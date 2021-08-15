5be824f0d09f24addc13b9bdfeee0dd92250f69b
IloLanga <37304497+IloLanga@users.noreply.github.com>
2021-08-15 10:21:06 +0200

    ADD buttons menu screen

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,12 @@ int main( int argc, char *argv[] )
 {
     Window window("Dice Game", 0, 0);
     window.drawIMG(0, 0, "fond.bmp");
+//    cout <<
+    window.drawIMG(600, 300, "start_button.bmp");
+    window.drawIMG(600, 425, "option_button.bmp");
+    window.drawIMG(600, 550, "exit_button.bmp");
     window.refresh();
 
-    Sleep(1000);
+    Sleep(2000);
     return 0;
 }
