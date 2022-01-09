31ecff1d740971be42e9385b48dd7005f34e6113
kavian <navid.kavian@ecole.ensicaen>
2022-01-09 20:24:21 +0100

    add requirement.h

---
+++ cmake-build-debug/CMakeFiles/Dice_Game_Project.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/navid/CLionProjects/ARPG_StoryBoard/src/Player.cpp" "CMakeFiles/Dice_Game_Project.dir/src/Player.cpp.obj" "gcc" "CMakeFiles/Dice_Game_Project.dir/src/Player.cpp.obj.d"
+  "C:/Users/navid/CLionProjects/ARPG_StoryBoard/src/Window.cpp" "CMakeFiles/Dice_Game_Project.dir/src/Window.cpp.obj" "gcc" "CMakeFiles/Dice_Game_Project.dir/src/Window.cpp.obj.d"
+  "C:/Users/navid/CLionProjects/ARPG_StoryBoard/src/WindowContent.cpp" "CMakeFiles/Dice_Game_Project.dir/src/WindowContent.cpp.obj" "gcc" "CMakeFiles/Dice_Game_Project.dir/src/WindowContent.cpp.obj.d"
+  "C:/Users/navid/CLionProjects/ARPG_StoryBoard/src/main.cpp" "CMakeFiles/Dice_Game_Project.dir/src/main.cpp.obj" "gcc" "CMakeFiles/Dice_Game_Project.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Dice_Game_Project.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dice_Game_Project.dir/src/Player.cpp.obj"
+  "CMakeFiles/Dice_Game_Project.dir/src/Player.cpp.obj.d"
+  "CMakeFiles/Dice_Game_Project.dir/src/Window.cpp.obj"
+  "CMakeFiles/Dice_Game_Project.dir/src/Window.cpp.obj.d"
+  "CMakeFiles/Dice_Game_Project.dir/src/WindowContent.cpp.obj"
+  "CMakeFiles/Dice_Game_Project.dir/src/WindowContent.cpp.obj.d"
+  "CMakeFiles/Dice_Game_Project.dir/src/main.cpp.obj"
+  "CMakeFiles/Dice_Game_Project.dir/src/main.cpp.obj.d"
+  "Dice_Game_Project.exe"
+  "Dice_Game_Project.exe.manifest"
+  "Dice_Game_Project.pdb"
+  "libDice_Game_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dice_Game_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
