70f1cc1dd269db06082938d756206ea479d3a770
kavian <navid.kavian@ecole.ensicaen>
2022-01-09 20:22:25 +0100

    add requirement.h

---
+++ include/requirement.h
@@ -0,0 +1,26 @@
+//
+// Created by navid on 18/08/2021.
+//
+
+#ifndef DICE_GAME_PROJECT_REQUIREMENT_H
+#define DICE_GAME_PROJECT_REQUIREMENT_H
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#ifdef __unix__
+#include <unistd.h>
+#endif
+
+#include <iostream>
+#include <memory>
+#include <unistd.h>
+#include <time.h>
+#include <set>
+
+#include <SDL2/SDL.h>
+#include <SDL_image.h>
+
+#define LOAD IMG_Load
+
+#endif //DICE_GAME_PROJECT_REQUIREMENT_H
