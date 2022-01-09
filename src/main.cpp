a17f168f7e19eca1d7dca84ebc32b65f40be53af
kavian <navid.kavian@ecole.ensicaen>
2022-01-09 13:49:20 +0100

    Changed project name

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 int main( int argc, char *argv[] )
 {
-    Window window("Dice Game", 700, 700);
+    Window window("ARPG_StoryBoard", 700, 700);
     window.drawIMG(0, 0, "fond.bmp");
     window.drawIMG(600, 300, "start_button.bmp");
     window.drawIMG(600, 425, "option_button.bmp");
